de3556435779069e04277d1c5aafe6a575951e06
HacktheMoon333 <52438706+HacktheMoon333@users.noreply.github.com>
2023-03-19 22:34:58 -0600

    Papyrus Commit

--- PapyrusAlpaca_Report.py
+++ PapyrusAlpaca_Report.py
@@ -3,32 +3,66 @@ import smtplib
 import ssl
 import schedule
 import time
+import json
+from datetime import datetime, timedelta
+
 import pandas as pd
 import numpy as np
 import requests
 import talib
-import json
+from tqdm import trange
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from finviz.screener import Screener
 from yahoo_fin import stock_info as si
 from alpaca_trade_api import REST
-from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
-# Load API keys from context file
-with open('context.json', 'r') as f:
-    context = json.load(f)
-
-# Initialize Alpaca API
-api_key = context['alpaca_api_key_id']
-api_secret = context['alpaca_api_secret_key']
-base_url = "https://paper-api.alpaca.markets" # paper trading
-api = REST(api_key, api_secret, base_url, api_version='v2')
+# Load environment variables from .env file
+load_dotenv()
 
 # Email settings
 smtp_server = "smtp.gmail.com"
 port = 465
-sender_email = context['sender_email']
-receiver_email = context['receiver_email']
-password = context['email_password']
+sender_email = os.getenv('SENDER_EMAIL')
+receiver_email = os.getenv('RECEIVER_EMAIL')
+password = os.getenv('EMAIL_PASSWORD')
+
+# Alpaca API initialization
+api_key = os.getenv('ALPACA_API_KEY_ID')
+api_secret = os.getenv('ALPACA_API_SECRET_KEY')
+base_url = os.getenv('BASE_URL')
+api = REST(api_key, api_secret, base_url, api_version='v2')
+
+# Function to send email
+def send_email(subject, body):
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = receiver_email
+    message['Subject'] = subject
+    message.attach(MIMEText(body, 'plain'))
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+        print(f"Email sent to {receiver_email} with subject '{subject}'")
+
+# Function to generate report and send email
+def generate_report_and_send_email():
+    # Generate the report here
+    report = "Report content"
+
+    # Send report email
+    send_email("Stock Report", report)
+
+# Function to send trade email
+def send_trade_email(trade_info):
+    # Generate trade email content
+    trade_email_body = f"Trade details:\n\n{trade_info}"
+
+    # Send trade email
+    send_email("Trade Executed", trade_email_body)
 
 # Calculate technical indicators (BB, RSI, SMA) from historical data
 def calculate_technical_indicators(historical_data):
@@ -100,15 +134,35 @@ def get_historical_data(symbol, period1, period2):
 
 # Function to execute trades
 def execute_trade():
-    # Read API keys from context.json
-    with open('context.json') as f:
-        context = json.load(f)
-		api_key = context['alpaca_api_key_id']
-		api_secret = context['alpaca_api_secret_key']
-        base_url = context['base_url']
-
-    # Initialize Alpaca API
+    # Initialize Alpaca API with environment variables
+    api_key = os.getenv('ALPACA_API_KEY_ID')
+    api_secret = os.getenv('ALPACA_API_SECRET_KEY')
+    base_url = os.getenv('BASE_URL')
     api = REST(api_key, api_secret, base_url, api_version='v2')
+
+
+    # ...
+    # Initialize progress bar
+    progress_bar = trange(3, desc="Executing trade", leave=True)
+    
+    # 1. Connect to SMTP server
+    progress_bar.set_description("Connecting to SMTP server")
+    context = ssl.create_default_context()
+    try:
+        server = smtplib.SMTP_SSL(smtp_server, port, context=context)
+        server.login(sender_email, password)
+    except Exception as e:
+        print(f"Error connecting to SMTP server: {e}")
+        return
+    progress_bar.update(1)  # Increment progress bar
+
+    # 2. Generate report and send email
+    progress_bar.set_description("Generating report and sending email")
+    generate_report_and_send_email()
+    progress_bar.update(1)  # Increment progress bar
+    
+    # 3. Calculate B-Score and display stock data
+    progress_bar.set_description("Calculating B-Score and displaying stock data")
 
     # ... (get stock data, calculate B-Score, and filter options as before)
 
@@ -118,6 +172,16 @@ def execute_trade():
 
     # Determine the amount to invest (20% of available funds)
     investment_amount = available_funds * 0.2
+    
+    # Display stock data
+    print("Stock data:")
+    print(stock_df)
+    
+    progress_bar.update(1)  # Increment progress bar
+    progress_bar.close()
+
+    # Generate report and send email
+    generate_report_and_send_email()
 
     # Make the trade
     if best_options:
@@ -134,19 +198,25 @@ def execute_trade():
                     limit_price=selected_option['option_price']
                 )
                 print(f"Placed order: {order}")
+
+                # Send trade email
+                send_trade_email(order)
+
             except Exception as e:
                 print(f"Error placing order: {e}")
         else:
             print("Not enough funds to place the order.")
     else:
         print("No suitable options found.")
+        
+def main():
+    # Schedule the execute_trade function to run every 4 hours
+    schedule.every(4).hours.do(execute_trade)
 
-# Schedule the execute_trade function to run every 4 hours
-schedule.every(4).hours.do(execute_trade)
+    # Run the scheduled tasks
+    while True:
+        schedule.run_pending()
+        time.sleep(60)  # Wait for 60 seconds before checking again
 
-# Run the scheduled tasks
-while True:
-    schedule.run_pending()
-    time.sleep(60)  # Wait for 60 seconds before checking again
-
-# print('b_score')
+if __name__ == "__main__":
+    main()
